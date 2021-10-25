3fb14c36b06f695c2ab31b1439e7dc6e69457f6d
Gengo Tanaka <gen0908@gmail.com>
2021-10-25 12:20:31 +0900

    #7 記事一覧の見栄えをちょっと修正

--- pages/index.js
+++ pages/index.js
@@ -11,17 +11,17 @@ export default function Home({ allPostsData }) {
         <title>gengogo5.com</title>
       </Head>
       <section className="">
-        <h2 className="">新着記事</h2>
+        <h2 className="py-3">記事一覧</h2>
         <ul className="">
           {allPostsData.map(({ id, date, title }) => (
-            <li className="" key={id}>
-              <Link href={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
+            <li className="text-lg pb-3" key={id}>
               <small className="text-gray-500">
                 <Date dateString={date} />
               </small>
+              <br />
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
             </li>
           ))}
         </ul>
