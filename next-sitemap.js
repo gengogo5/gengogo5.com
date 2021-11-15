ec0be6df25acab1b7f6c9fe65123ff8ee9cee764
Gengo Tanaka <gen0908@gmail.com>
2021-11-16 08:48:54 +0900

    #19 ルート除外を諦める

--- next-sitemap.js
+++ next-sitemap.js
@@ -2,6 +2,5 @@ module.exports = {
   siteUrl: process.env.HOST || 'https://gengogo5.com',
   changefreq: 'weekly',
   generateRobotsTxt: true,
-  exclude: ['https://gengogo5.com'],
   outDir: './out'
 }
