ca2ac49c957e2c364f8d49f4dd722b6bcf9e82b6
Gengo Tanaka <gen0908@gmail.com>
2021-11-23 15:18:53 +0900

    #20 カテゴリ別のパス生成コードを追加

---
+++ pages/categories/[category].js
@@ -0,0 +1,58 @@
+import Layout from '../../components/layout'
+import { getAllCategories, getSortedPostsData } from '../../lib/posts'
+import Head from 'next/head'
+import Link from 'next/link'
+import Date from '../../components/date'
+import React from 'react';
+
+export default function Home({ allPostsMetaData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>gengogo5.com</title>
+      </Head>
+      <section className="pt-5">
+        <ul>
+          {allPostsMetaData.map(({ id, summary, date, title }) => (
+            <li className="border-b border-indigo-100 pt-1" key={id}>
+              <Link href={`/posts/${id}`}>
+                <a className="block pb-6 hover:opacity-60">
+                  <div className="text-gray-400 text-sm">
+                    <Date dateString={date} />
+                  </div>
+                  <span className="text-lg font-semibold">{title}</span>
+                  <div className="text-sm text-gray-500">{summary}</div>
+                </a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  )
+}
+
+export async function getStaticPaths() {
+  const paths = await getAllCategories()
+
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const allPostsData = await getSortedPostsData(params?.category)
+
+  const allPostsMetaData = allPostsData.map((data) => {
+    const { contentHtml, ...metaData } = data
+    return metaData
+  })
+
+  return {
+    props: {
+      allPostsMetaData
+    }
+  }
+}
+
