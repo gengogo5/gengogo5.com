4569326e3eb005a7bf676edf272d3e791711d970
Gengo Tanaka <gen0908@gmail.com>
2021-11-23 17:35:30 +0900

    #20 カテゴリリンクのエフェクト変更

--- components/indexItem.js
+++ components/indexItem.js
@@ -8,13 +8,13 @@ export default function IndexItem(props) {
         <span className="text-gray-400 text-sm">
           <Date className="text-gray-400 text-sm" dateString={props.date} />
         </span>
-        <span className="ml-1 px-1 border rounded-lg border-solid bg-gray-100">
+        <span className="ml-1 px-1 border rounded-lg border-solid bg-gray-100 hover:bg-gray-200">
           <Link href={`/categories/${props.category}`}>
             <a className="text-sm text-gray-700">{props.category}</a>
           </Link>
         </span>
         <Link href={`/posts/${props.id}`}>
-          <a className="block pb-6 hover:opacity-60">
+          <a className="block pb-6 hover:underline">
             <span className="text-lg font-semibold">{props.title}</span>
             <div className="text-sm text-gray-500">{props.summary}</div>
           </a>
