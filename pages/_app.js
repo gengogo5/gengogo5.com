b512a002aeced65c3de6c7a190693b9a2d027706
Gengo Tanaka <gen0908@gmail.com>
2021-11-27 16:48:27 +0900

    #46 サイト内ページ遷移を計測できるようにする

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,20 @@
+import { GA_TRACKING_ID, pageview } from 'lib/gtag';
 import '../styles/global.css'
 import 'zenn-content-css';
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    if (!GA_TRACKING_ID) return;
+
+    const handleRouteChange = (url) => {
+      pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return <Component {...pageProps} />
 }
